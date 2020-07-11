9675f3d6850765c8fa9beaf077258f59d548f811
fil0157 <56268081+fil0157@users.noreply.github.com>
2020-07-11 20:00:02 +0400

    Fix app themes

--- src/app/themes/dark.ts
+++ src/app/themes/dark.ts
@@ -7,6 +7,7 @@ export const DarkTheme: Theme = {
     identifier: 'darkTheme',
     values: {
     '--color': '#fff',
+    '--border': '#ddd',
     '--button-color': '#fff',
     '--background': '#3e3e3e',
     '--button-background': '#4358a8',
--- src/app/themes/light.ts
+++ src/app/themes/light.ts
@@ -6,9 +6,10 @@ import { Theme } from '@fil0157/ngx-themes';
 export const LightTheme: Theme = {
     identifier: 'lightTheme',
     values: {
-    '--color': '#000',
+    '--color': '#08f',
+    '--border': '#ddf',
     '--button-color': '#000',
-    '--background': '#fff',
+    '--background': '#eff',
     '--button-background': '#a7bfc2',
     }
 };
